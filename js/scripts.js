c77d6a434e418c5db4fbb9e1572b03976a41b298
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-06-11 13:37:31 -0700

    Update 'type:' from string to array in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  { name: 'Bulbasaur', height: 7, type: 'grass//poison'},
-  { name: 'Squirtle', height: 5, type: 'water'},
-  { name: 'Charmander', height: 6, type: 'fire'},
-  { name: 'Pikachu', height: 4, type: 'electric'}
+  { name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+  { name: 'Squirtle', height: 5, type: ['water']},
+  { name: 'Charmander', height: 6, type: ['fire']},
+  { name: 'Pikachu', height: 4, type: ['electric']}
 ];
