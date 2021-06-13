770c849c49f5f36559491904bceb3dc1430226be
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-06-12 21:34:50 -0700

    Updated let variable to const variable and added loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
-let pokemonList = [
+const pokemonList = [
   { name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
   { name: 'Squirtle', height: 5, type: ['water']},
   { name: 'Charmander', height: 6, type: ['fire']},
   { name: 'Pikachu', height: 4, type: ['electric']}
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  //Prints pokemon name to DOM
+  document.write(pokemonList[i].name + " (height :" + pokemonList[i].height + ")");
+  //Checks if pokemon height is greater than 6
+  if (pokemonList[i].height > 6) {
+    document.write(" -Wow, that's big!");
+  }
+  //Adds breaks between items
+  document.write("<br>")
+}
