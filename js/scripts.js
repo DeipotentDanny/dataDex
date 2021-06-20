2e554566b5959779685f42707ca84c6a556fd4e9
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-06-19 23:06:25 -0700

    Updated scripts.js
    -Added const "pokemonRepository" and wrapped code in IIFE
    -updated "for" loop to "forEach" function
    -made "add(pokemon)" verifiable
    -validated functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,49 @@
-const pokemonList = [
-  { name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-  { name: 'Squirtle', height: 5, type: ['water']},
-  { name: 'Charmander', height: 6, type: ['fire']},
-  { name: 'Pikachu', height: 4, type: ['electric']}
-];
+//IIFE
+const pokemonRepository = (function () {
+  //main array
+  const pokemonList = [
+    { name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    { name: 'Squirtle', height: 5, type: ['water']},
+    { name: 'Charmander', height: 6, type: ['fire']},
+    { name: 'Pikachu', height: 4, type: ['electric']}
+  ];
 
-for (let i=0; i < pokemonList.length; i++) {
-  //Prints pokemon name to DOM
-  document.write(pokemonList[i].name + " (height :" + pokemonList[i].height + ")");
-  //Checks if pokemon height is greater than 6
-  if (pokemonList[i].height > 6) {
-    document.write(" -Wow, that's big!");
+  //public function
+  function add(pokemon) {
+    if (typeof pokemon === 'object' &&
+      'name' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Pokemon input incorrect')
+    }
   }
-  //Adds breaks between items
-  document.write("<br>")
-}
+
+  //public function
+  function getAll() {
+    return pokemonList;
+  }
+
+  //accessible keys
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+//validation of IIFE and code functionality
+pokemonRepository.add({ height: '32' }); //should return Pokemon input incorrect in console
+console.log(pokemonRepository.getAll());
+
+//Function to validate and write data to DOM
+pokemonRepository.getAll().forEach(function(list) {
+    //Prints pokemon data to DOM
+    document.write("<p>" + list.name + " (height :" + list.height + ")" + "</p>");
+    //Checks if pokemon height is greater than 6
+    if (list.height > 6) {
+      document.write("<p> -Wow, that's big! </p>");
+    }
+    //Prints the array to the console for validation
+    console.log(list.name);
+});
